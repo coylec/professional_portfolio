1d6cf25d7b85ceb47f94e765cf9f9e389373bbf3
Conrad Coyle <coylec@users.noreply.github.com>
2019-02-16 17:45:41 +0000

    update allowed hosts

--- professional_portfolio/settings/base.py
+++ professional_portfolio/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('PROJECT_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'outsiderdevworks.com.herokudns.com', '.outsiderdevworks.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'outsiderdev-portfolio.herokuapp.com', '.outsiderdevworks.com']
 
 
 # Application definition
