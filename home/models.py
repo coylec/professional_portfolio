2858fbb528f40fe7de5eafbc8237473deee5e3d2
Conrad Coyle <coylec@users.noreply.github.com>
2019-02-16 15:09:48 +0000

    add new models and content updates

--- home/models.py
+++ home/models.py
@@ -6,3 +6,4 @@ class Job(models.Model):
     title = models.CharField(max_length=50, default="Example Work")
     image = models.ImageField(upload_to='images/')
     summary = models.TextField()
+    web_address = models.URLField(max_length=200)
